5165621adc3567b4581c406a7b74c9ac39bebbc7
sayyidyofa <sayyidyofa@gmail.com>
2025-01-10 23:02:54 +0700

    (fix sh) pipeline for deploy backend on release

--- backend/deploy/container-ikapiar.sh
+++ backend/deploy/container-ikapiar.sh
@@ -1,5 +1,5 @@
 podman ps -q --filter "name=ikapiar-backend" | grep -q . && \
-podman stop ikapiar-backend && \
-podman rm ikapiar-backend && \
+podman stop ikapiar-backend
+podman rm ikapiar-backend || true
 podman pull ghcr.io/ikapiar/backend:latest && \
 podman run -d --name ikapiar-backend --restart always --pull always --env-file /apps/ikapiar-backend/env -p 4000:4000 ghcr.io/ikapiar/backend:latest
